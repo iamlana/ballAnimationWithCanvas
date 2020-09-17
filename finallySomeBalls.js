f2e261f8063b49b84fbc23795f467879b889acca
Anna Lukianova <ann.lookyanova@gmail.com>
2020-09-17 14:37:02 +0200

    Make ball move circulary

--- finallySomeBalls.js
+++ finallySomeBalls.js
@@ -1,56 +1,30 @@
-import { Rectangle } from './Rectangle.js'
-
 export function finallySomeBalls() {
   const canvas = document.getElementById('gameCanvas')
   const ctx = canvas.getContext('2d')
-  function createGrid() {
-    // draw a line every *step* pixels
-    const step = 50
+  let x = canvas.width / 2;
+  let y = canvas.height / 6;
+  let ballRadius = 10;
+  let radians = 0;
 
-    // our end points
-    const width = canvas.width
-    const height = canvas.height
 
-    ctx.save()
-    ctx.strokeStyle = 'gray' // line colors
-    ctx.fillStyle = 'black'
-    ctx.font = '14px Monospace'
-    ctx.lineWidth = 0.39
 
-    for (let x = 0; x <= width; x += step) {
-      ctx.fillText(x, x, 12)
-    }
-
-    for (let y = 0; y <= height; y += step) {
-      ctx.fillText(y, 0, y)
-    }
-
-    for (let x = 0; x < width; x += step) {
-      for (let y = 0; y < height; y += step) {
-        drawCircle(ctx, x, y, 1, '#aaa')
-      }
-    }
-
-    ctx.restore()
+  function move() {
+    radians += 0.05;
+    x = x + Math.cos(radians) * 10;
+    y = y + Math.sin(radians) * 10;
+    // ctx.clearRect(0, 0, canvas.width, canvas.height)
+    drawBall();
   }
 
-  createGrid()
+  function drawBall() {
+    ctx.beginPath();
+    ctx.arc(x, y, ballRadius, 0, Math.PI * 2, false);
+    ctx.fillStyle = 'gold';
+    ctx.fill();
+    ctx.closePath();
+  }
 
+  // window.requestAnimationFrame(move)
+  setInterval(move, 50);
 }
 
-/**
- * @param {CanvasRenderingContext2D} ctx
- * @param {number} x
- * @param {number} y
- * @param {number} radius
- * @param {string} color
- */
-function drawCircle(ctx, x, y, radius, color) {
-  ctx.save()
-  ctx.fillStyle = color
-  ctx.beginPath()
-  ctx.arc(x, y, radius, 0, 2 * Math.PI)
-  ctx.closePath()
-  ctx.fill()
-  ctx.restore()
-}
